e4b7f608710e60150bd95c51dcc8e433a83c7db8
Timothy Morton <tim.morton@gmail.com>
2015-01-08 11:44:10 -0800

    fixed typos

--- periodogram/timeseries.py
+++ periodogram/timeseries.py
@@ -5,36 +5,40 @@ Base class for time series
 
 import numpy as np
 
-class TimeSeries(self, t, f, df=None, mask=None):
-    """
-    Base clase for time series data
+class TimeSeries(object):
+    def init(self, t, f, df=None, mask=None):
+        """
+        Base clase for time series data
 
-    Parameters
-    ----------
-    t : array_like
-        times
+        Parameters
+        ----------
+        t : array_like
+            times
 
-    f : array_like
-        fluxes, must be same length as times
+        f : array_like
+            fluxes, must be same length as times
 
-    df : float array_like, optional
-        uncertainties; if float, then all assumed to be the same;
-        
+        df : float array_like, optional
+            uncertainties; if float, then all assumed to be the same;
+            if array, then must be same length as times and fluxes
 
-    mask : array_like or 
-    """
+        mask : array_like or None
+        """
 
-    assert(t.shape == f.shape)
+        assert(t.shape == f.shape)
+
+        self.t = t
+        self.f = f
+        if df is not None:
+            if np.size(df)==1:
+                df = np.ones_like(f) * df
+            else:
+                assert(df.shape == f.shape)
+        self.df = df
+
+        if mask is None:
+            mask = np.isnan(f)
+        self.mask = mask
+
     
-    self.t = t
-    self.f = f
-    if df is not None:
-        if np.size(df)==1:
-            df = np.ones_like(f) * df
-        else:
-            assert(df.shape == f.shape)
-    self.df = df
-
-    if mask is None:
-        mask = np.isnan(f)
-    self.mask = mask
+    
