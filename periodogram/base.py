2f22acc4d15743539dbc0770983d5d5c7e00b37f
Jake VanderPlas <jakevdp@gmail.com>
2015-01-08 10:27:02 -0800

    initial package framework

---
+++ periodogram/base.py
@@ -0,0 +1,25 @@
+"""
+Base class for models of periodic sources
+"""
+
+
+class PeriodicModeler(object):
+    """Base class for periodic modeling"""
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("PeriodicModeler")
+
+    def fit(self, t, y, dy=None, filts=None):
+        """Provide data for the fit"""
+        raise NotImplementedError()
+
+    def predict(self, t, filts=None, period=None):
+        """Predict the best-fit model at t for the given period"""
+        raise NotImplementedError()
+
+    def score(self, period):
+        """Compute the score for a period or array of periods"""
+        raise NotImplementedError()
+
+    def period_search(self):
+        """Find the best period for the model"""
+        # TODO: implement using the score() function provided by subclasses
