c88fdf9ad0f964533eb153d53c0708948fa8aba4
Timothy Morton <tim.morton@gmail.com>
2015-01-11 00:57:02 -0500

    implemented ACF, with test

--- periodogram/acf.py
+++ periodogram/acf.py
@@ -6,9 +6,8 @@ from __future__ import print_function,division
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.interpolate import UnivariateSpline as interpolate
 from scipy.ndimage.filters import gaussian_filter
-from scipy.ndimage import median_filter
-from scipy.optimize import leastsq, curve_fit
 
 import logging
 
@@ -19,170 +18,70 @@ from .base import PeriodicModeler
 
 
 
-class acf(PeriodicModeler):
+class ACF(PeriodicModeler):
     """
-    Autocorrelation Function for periodic modeling
+    Autocorrelation function for periodic modeling.
 
-    kwargs
-    ------
-    smooth=18
-
-    days=True
-
-    
+    This can only deal with evenly sampled data.  Use ACFUneven
+    for implementation of Scargle (1989) ACF for uneven time sampling.
 
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, maxlag=None, smooth=None):
+        self.maxlag = maxlag
+        self.smooth = smooth
 
-        # Set cadence (use astropy.units)
-        self.cadence = 0.02043423 #d #PLACEHOLDER
-        self.default_maxlag = 50//self.cadence
-
-        #set private variables for cached acorr calculation
-        self._lag = None  #always should be in cadences
-        self._ac = None 
-
+        self.power_fn = None
+        
     def fit(self, t, y, dy=None, filts=None):
-        """Provide data for the fit"""
+        """Data must be evenly sampled.
+        """
+        
+        #TODO: add real test to see if data is evenly sampled with no gaps
+        # for now, just assume it is 
+        
         self.t = np.atleast_1d(t)
         self.y = np.atleast_1d(y)
-        self.dy = dy
+        self.dy = dy #ignored
 
-        mask = np.isnan(self.y)
-        self.mask = mask
-        self.y = self.y[~self.mask]
+        if self.maxlag is None:
+            maxlag = len(self.y)
+        else:
+            maxlag = self.maxlag
+        
+        self.cadence = np.median(self.t[1:] - self.t[:-1])
+        
+        self.ac = acor.function(self.y, maxlag)
+        if self.smooth is not None:
+            ac = gaussian_filter(ac, smooth)
+        
+        self.lag = np.arange(maxlag) * self.cadence
 
-    def predict(self, t, filts=None, period=None):
-        """Predict the best-fit model at t for the given period"""
-        raise NotImplementedError()
+        self.power_fn = interpolate(self.ac, self.lag, s=0, k=1)
 
     def score(self, period):
         """Compute the score for a period or array of periods"""
-        
+        if self.power_fn is None:
+            raise RuntimeError('Must fit before calculating score')
 
-    def period_search(self, period=None, fit_npeaks=4,
-                   smooth=18, maxlag=None, lookahead=5,
-                   tol=0.2, return_peaks=False):
-        """Find the best period for the model"""
-        # This was acorr_period_fit
+        return self.power_fn(period)
+                
 
-        peaks, lphs, hts = self.acorr_peaks(smooth=smooth, maxlag=maxlag,
-                                            lookahead=lookahead, return_heights=True)
-        logging.debug(peaks)
-        logging.debug(lphs)
-        logging.debug(hts)
+    def period_search(self, lookahead=5):
+        """Finds most promising peak; returns as best period. 
+        """
 
-        firstpeak = peaks[0]
-### This is having trouble with the test cases (second peak is only 0.01 higher, but it's not the input
-#        if lphs[0] >= lphs[1]:
-#            firstpeak = peaks[0]
-#        else:
-#            firstpeak = peaks[1]
-#            if lphs[1] < 1.2*lphs[0]:
-#                logging.warning('Second peak (selected) less than 1.2x height of first peak.')
+        peaks, lphs, hts = peaks_and_lphs(self.ac, self.lag, return_heights=True,
+                                          lookahead=lookahead)
 
-        if period is None:
-            period = firstpeak
+        #Return first peak, unless second peak is more than 5% higher (arbitrary, granted)
+        if lphs[1] > 1.05*lphs[0]:
+            logging.warning('ACF period_search: second peak ({0[1]}) is more than 5% higher than first ({0[0]}); using second'.format(lphs))
+            return peaks[1]
 
-        if fit_npeaks > len(peaks):
-            fit_npeaks = len(peaks)
-        #peaks = peaks[:fit_npeaks]
-
-        #identify peaks to use in fit: first 'fit_npeaks' peaks closest to integer
-        # multiples of period guess
-
-        fit_peaks = []
-        fit_lphs = []
-        fit_hts = []
-        last = 0.
-        #used = np.zeros_like(peaks).astype(bool)
-        for n in np.arange(fit_npeaks)+1:
-            #find highest peak within 'tol' of integer multiple (that hasn't been used)
-            close = (np.absolute(peaks - n*period) < (tol*n*period)) & ((peaks-last) > 0.3*period)
-            if close.sum()==0:
-                fit_npeaks = n-1
-                break
-                #raise NoPeakError('No peak found near {}*{:.2f}={:.2f} (tol={})'.format(n,period,n*period,tol))
-            ind = np.argmax(hts[close])
-            last = peaks[close][ind]
-            fit_peaks.append(peaks[close][ind])
-            fit_lphs.append(lphs[close][ind])
-            fit_hts.append(hts[close][ind])
-            #used[close][ind] = True
-            logging.debug('{}: {}, {}'.format(n*period,peaks[close],peaks[close][ind]))
-            #logging.debug(used)
-
-            #ind = np.argmin(np.absolute(peaks - n*period)) #closest peak
-            #fit_peaks.append(peaks[ind])
-            #fit_lphs.append(lphs[ind])
-
-        logging.debug('fitting peaks: {}'.format(fit_peaks))
-
-        if fit_npeaks < 3:
-            return peaks,-1, fit_peaks, fit_lphs, fit_hts
-
-
-        x = np.arange(fit_npeaks + 1)
-        y = np.concatenate([np.array([0]),fit_peaks])
-
-        #x = np.arange(fit_npeaks) + 1
-        #y = fit_peaks
-
-        def fn(x,a,b):
-            return a*x + b
-
-        fit,cov = curve_fit(fn, x, y, p0=(period,0))
-        if return_peaks:
-            return fit[0],cov[0][0],fit_peaks,fit_lphs,fit_hts
         else:
-            return fit[0],cov[0][0]
-
-
-        # TODO: implement using the score() function provided by subclasses  ############
-
+            return peaks[0]
         
-
-    def acorr(self, maxlag=None, recalc=False, **kwargs):
-
-        smooth = kwargs.get("smooth",18)
-        days = kwargs.get("days",True)
-
-        if maxlag is None: 
-            maxlag = self.default_maxlag
-
-        
-        if self._ac is not None and not recalc:
-            lag = self._lag
-            ac = self._ac
-        else:
-            x = self.y.copy()
-            x[self.mask] = 0
-
-            #logging.debug('{} nans in x'.format((np.isnan(x)).sum()))
-
-            ac = acor.function(x, maxlag)
-            lag = np.arange(maxlag)
-
-            #smooth AC function
-            ac = gaussian_filter(ac, smooth)
-
-            #set private variables for cached calculation
-            self._ac = ac
-            self._lag = lag
-            self._maxlag = maxlag
-            self._smooth = smooth
-
-        if days:
-            return lag*self.cadence,ac
-        else:
-            return lag,ac
-
-    def acorr_peaks(self, lookahead=5, days=True, 
-                    return_heights=False, **kwargs):
-        lag, ac = self.acorr(**kwargs)
-        return peaks_and_lphs(ac, lag, return_heights=return_heights,
-                              lookahead=lookahead)
         
 
 
--- periodogram/tests/test_acf.py
+++ periodogram/tests/test_acf.py
@@ -1,48 +1,28 @@
+import numpy as np
+from numpy.testing import assert_allclose, assert_array_less, assert_equal
+
 import logging
 
-import numpy as np
 import matplotlib.pyplot as plt
 
-from periodogram import acf
+from ..acf import ACF
 
-logging.basicConfig(level=logging.WARNING)
+def make_sine(N=1000, P=100, err=0.05, rseed=None):
+    rng = np.random.RandomState(rseed)
+    
+    t = np.arange(N) #evenly-sampled data
+    y = np.ones(len(t))
+    
+    y *= np.sin(2*np.pi*t/P)
+    y += rng.randn(N)*err
+    
+    return t,y
 
-def make_sine(xmin=1,xmax=100,cadence=0.02043423,shifts=[0],amplitudes=[1.]):
-    x = arange(xmin,xmax,cadence)
-    y = np.ones(len(x))
-    for i,shift in enumerate(shifts):
-        y *= np.sin(x + shift)*amplitudes[i]
-    y += np.random.random(len(x))*0.5
-    return x,y
-
-def plot_test(x,y,input_period,fit_period):
-    plt.figure()
-    ax = plt.subplot(111)
-    ax.plot(x,y,"k.")
-    for per in np.arange(1,ax.get_xlim()[1],input_period):
-        ax.plot([per,per],ax.get_ylim(),"g--")
-    for per in np.arange(1,ax.get_xlim()[1],fit_period):
-        ax.plot([per,per],ax.get_ylim(),"r-")
-
-def test_single_sine():
-    x,y = make_sine(xmax=60)
-    a = acf.acf()
-    a.fit(x,y)
-    period,period_err = a.period_search()
-    print "Input = 2*pi = {}".format(2*np.pi)
-    print period,period_err
-    plot_test(x,y,2*np.pi,period)
-
-def test_double_sine():
-    x,y = make_sine(xmax=60,shifts=[0.,np.pi/5.],amplitudes=[1.,0.9])
-    a = acf.acf()
-    a.fit(x,y)
-    period,period_err = a.period_search()
-    print "Input = 2*pi = {}".format(2*np.pi)
-    print period,period_err
-    plot_test(x,y,2*np.pi,period)
+def test_single_sine(P=100):
+    t,y = make_sine(P=P)
+    a = ACF()
+    a.fit(t,y)
+    pbest = a.period_search()
+    assert_allclose(pbest, P, 0.01)
 
 
-plt.close("all")
-test_single_sine()
-test_double_sine()
